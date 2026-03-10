64f78230a4d6b3aad43f678fe6d75926ba52e093
Roua <rouahammami14@gmail.com>
2026-03-10 10:15:38 +0100

    adding frontend

---
+++ src/utils/axioInstance.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+const BASE_URL = "https://server-79st.onrender.com/api"
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+})
+
+export default axiosInstance
